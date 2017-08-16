599d839ab5a7a867de2ba32c2256c5f887f1f41d
Kane <Phoboes@live.com>
2017-08-16 17:01:37 +1000

    Muscle filled.
    Illustrator... Illustrator never changes...

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ window.onload = function(){
   // console.log("Loaded.")
   const paths = document.querySelectorAll('path');
 
-  for( let i = 0; i < paths.length; i++ ){
+  for( let i = paths.length - 1; i >= 0; i-- ){
     // debugger
     window.setTimeout( function(){
       // debugger
       paths[i].classList.add('animate');
-    }, i );
+    }, ( i ) );
     // console.log(paths[i])
   }
 }
